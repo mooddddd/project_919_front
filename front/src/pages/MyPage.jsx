9b5fc9136c4f9f1085c33c0317c8f65cc765d115
Hynn <116252978+Hynn-Hyunwoong@users.noreply.github.com>
2023-04-10 18:04:55 +0900

    fix:error

--- front/src/pages/MyPage.jsx
+++ front/src/pages/MyPage.jsx
@@ -1,7 +1,5 @@
 import { Header, Footer } from '../common'
 import { MyPageContent } from './index'
-// import { useNavigate } from 'react-router-dom'
-// import { useSelector } from 'react-redux'
 
 export const MyPage = () => {
   return (
--- front/src/pages/content/MyPageContent.jsx
+++ front/src/pages/content/MyPageContent.jsx
@@ -6,11 +6,7 @@ import {
   MyHeaderLogo,
   MyHeaderWrap,
   MyBodyWrapper,
-  MyInfoData,
-  MyInfo,
-  MyInfoBtn,
 } from '../styled'
-
 import { MyInfo, MyList } from './mypage'
 
 export const MyPageContent = () => {
@@ -18,41 +14,29 @@ export const MyPageContent = () => {
   const getMyInfoHandler = (e) => {
     setMenu(true)
   }
-  const getMyListHandler = (e) => {
+  const geyMyListHandler = (e) => {
     setMenu(false)
   }
 
-  const infoData = myInfo // 엑시오스로 가져와서 상태변경
-
   return (
     <>
-    <MyWrapper>
-      <MyBackground>
-        <MyHeaderWrap>
-          <MyHeaderLogo>
-            My Page
-          </MyHeaderLogo>
-        </MyHeaderWrap>
-        <MyBodyWrapper>
-          <MyInfoData>
-          {menu ? (
-            <MyInfoView infoData={infoData} setMyInfo={setMyInfo} />
-            ) : (
-              <MyList infoData={infoData} setMyInfo={setMyInfo}/>
-              )}
-          </MyInfoData>
-          <MyInfo>
-            <MyInfoBtn>
-              <button onClick={getMyInfoHandler}> 정보수정 </button>
-            </MyInfoBtn>
-            <MyInfoBtn>
-              <button onClick={getMyListHandler}> 파티관리 </button>
-            </MyInfoBtn>
-          </MyInfo>
-        </MyBodyWrapper>
-      </MyBackground>
-    </MyWrapper>
+      <MyWrapper>
+        <MyBackground width="90%">
+          <MyHeaderWrap>
+            <MyHeaderLogo>My Page</MyHeaderLogo>
+          </MyHeaderWrap>
+
+          <MyBodyWrapper>
+            <div className="nav">
+              <ul>
+                <li onClick={getMyInfoHandler}>정보 수정</li>
+                <li onClick={geyMyListHandler}>파티 관리</li>
+              </ul>
+            </div>
+            {menu ? <MyInfo /> : <MyList />}
+          </MyBodyWrapper>
+        </MyBackground>
+      </MyWrapper>
     </>
   )
 }
-
