613a45960c693bfb774d631c4de749b9f999d0a9
mood <mooddddd@naver.com>
2023-04-10 13:50:27 +0900

    fix: 마이페이지 수정 이미지 이슈 수정 완료

--- front/src/pages/content/mypage/MyList.jsx
+++ front/src/pages/content/mypage/MyList.jsx
@@ -1,27 +1,58 @@
 import { useState, useEffect } from 'react'
 import { request } from '../../../utils'
 import { getCookie } from '../../../utils'
+import { NavLink } from 'react-router-dom'
 
-const publicPath = process.env.PUBLIC_URL
-
-export const MyList = ({ listData }) => {
-  const [myList, setMyList] = useState('리스트 테스트')
+export const MyList = () => {
+  const [myList, setMyList] = useState(['리스트 테스트'])
+  const [myLike, setMyLike] = useState(['조아요 테스트'])
+  const [myPost, setMyPost] = useState(['등록된 글이 없습니다'])
 
   const token = getCookie('token')
 
   useEffect(() => {
     ;(async () => {
       try {
-        const data = await request.get('user/mylist', {
+        const { data } = await request.get('user/mylist', {
           headers: { Authorization: `Bearer ${token}` },
         })
-        console.log(data)
+        const { myList, myLike, myPost } = data
+        setMyList(myList)
+        setMyLike(myLike)
+        setMyPost(myPost)
       } catch (e) {
         throw new Error(e)
       }
     })()
   }, [])
-  // 파티는 어차피 많아봤자 몇 개 없을거니까 한꺼번에 가져와도 ㄱㅊ을 것 같음
 
-  return <>mylist : {listData}</>
+  const listFunc = (list) => {
+    const mapped = list.map((v) => {
+      return (
+        <div key={v.recruitIndex}>
+          <NavLink to={`/community/recruit/view/${v.recruitIndex}`}>
+            {v.title}
+          </NavLink>
+        </div>
+      )
+    })
+    return mapped
+  }
+
+  return (
+    <>
+      <div>
+        <span>내가 쓴 글 :</span>
+        {listFunc(myPost)}
+      </div>
+      <div>
+        <span>내가 참여한 글 :</span>
+        <div>{listFunc(myList)}</div>
+      </div>
+      <div>
+        <span>관심있는 글 :</span>
+        <div>{listFunc(myLike)}</div>
+      </div>
+    </>
+  )
 }
