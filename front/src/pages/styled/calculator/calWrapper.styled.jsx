4d7cffcbb45886f2b366da0000b165479c7212e1
mood <mooddddd@naver.com>
2023-04-11 14:26:16 +0900

    feat: 계산기 페이지 완료

--- front/src/pages/styled/calculator/calWrapper.styled.jsx
+++ front/src/pages/styled/calculator/calWrapper.styled.jsx
@@ -1,9 +1,25 @@
 import styled from 'styled-components'
+const publicPath = process.env.PUBLIC_URL
 
 export const CalWrapper = styled.div`
-  margin: 0 auto;
-  width: 80%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+  background-image: url(${publicPath}/img/pinkcloud.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  opacity: 0.85;
+
+  & .content {
+    margin: 0 auto;
+    width: 80%;
+    background: rgba(255, 255, 255, 0.3);
+
+    & .sort {
+      width: 90%;
+      margin: 0 auto;
+      padding: 3rem 0;
+    }
+  }
 `
+
+/* display: flex; */
+/* flex-direction: row; */
+/* align-items: center; */
---
+++ front/src/pages/styled/calculator/text.styled.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const SmallP = styled.p`
+  font-size: 0.8rem;
+  color: #666666;
+  margin-top: 0.2rem;
+`
+
+export const BigP = styled.p`
+  font-size: 1.2rem;
+  font-weight: bold;
+`
+
+export const SuperSpan = styled.span`
+  font-size: 2rem;
+  font-weight: bold;
+  color: #ff5858;
+`
+
+export const SmallSpan = styled.span`
+  font-size: 0.8rem;
+`
+
+export const BigSpan = styled.span`
+  font-size: 1.2rem;
+  color: #ff5858;
+  font-weight: bold;
+`
