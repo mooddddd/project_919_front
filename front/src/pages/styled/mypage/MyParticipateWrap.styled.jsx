44d191e3b2217570468c2ee3ee58078be78296d3
mood <mooddddd@naver.com>
2023-04-14 09:58:57 +0900

    fix 내정보 살짝 수정

--- front/src/pages/styled/mypage/MyParticipateWrap.styled.jsx
+++ front/src/pages/styled/mypage/MyParticipateWrap.styled.jsx
@@ -1,12 +1,11 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
 export const MyParticipateWrap = styled.div`
-    width: 498px;
-    height: 200px;
-    color: #161616;
-    font-size: 1.3rem;
-    background-color: transparent;
-    color: #252525;
-    border: 1px solid #fff;
-    text-align: center;
+  width: 498px;
+  color: #161616;
+  font-size: 1.3rem;
+  background-color: transparent;
+  color: #252525;
+  border: 1px solid #fff;
+  text-align: center;
 `
--- front/src/pages/styled/mypage/PartyWrapper.styled.jsx
+++ front/src/pages/styled/mypage/PartyWrapper.styled.jsx
@@ -1,7 +1,7 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
 export const PartyWrapper = styled.div`
-    width: 500px;
-    height: 700px;
-    margin: 0 auto;
+  width: 500px;
+  /* margin: 0 auto; */
+  margin: 0 auto 5rem;
 `
